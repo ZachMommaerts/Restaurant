c7caa255c4c6dae99132896ba896b83f146f5548
ZachMommaerts <zmommaerts@gmail.com>
2023-07-14 23:00:56 -0500

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Zach Mommaerts on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
