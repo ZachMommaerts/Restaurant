f54874c0054ffa725a39243e013f770d1a0531ec
ZachMommaerts <zmommaerts@gmail.com>
2023-07-17 13:27:07 -0500

    - Add Photo and back button to PhotoView Sheet

--- Restaurant/GalleryView.swift
+++ Restaurant/GalleryView.swift
@@ -11,6 +11,7 @@ struct GalleryView: View {
     
     @State var photoData = [String]()
     @State var sheetVisible = false
+    @State var selectedPhoto = ""
     var dataService = DataService()
     
     var body: some View {
@@ -32,6 +33,7 @@ struct GalleryView: View {
                                 .frame(maxWidth: (proxy.size.width-20)/3)
                                 .clipped()
                                 .onTapGesture {
+                                    selectedPhoto = photo
                                     sheetVisible = true
                                 }
                         }
@@ -44,7 +46,8 @@ struct GalleryView: View {
             photoData = dataService.getPhotos()
         }
         .sheet(isPresented: $sheetVisible) {
-            PhotoView()
+            PhotoView(selectedPhoto: $selectedPhoto,
+                      sheetVisible: $sheetVisible)
         }
     }
 }
--- Restaurant/PhotoView.swift
+++ Restaurant/PhotoView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct PhotoView: View {
+    
+    @Binding var selectedPhoto: String
+    @Binding var sheetVisible: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Image(selectedPhoto)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            VStack{
+                HStack {
+                    Spacer()
+                    Button {
+                        sheetVisible = false
+                    } label: {
+                        Image(systemName: "x.circle")
+                            .foregroundColor(.black)
+                            .scaleEffect(2)
+                    }
+                    .padding()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
 struct PhotoView_Previews: PreviewProvider {
     static var previews: some View {
-        PhotoView()
+        PhotoView(selectedPhoto: Binding.constant("gallery1"),
+                  sheetVisible: Binding.constant(true))
     }
 }
